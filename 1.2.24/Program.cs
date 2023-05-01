7e9ea254d3bcc53ee5194ecec5372032c4c0456d
JustKrs <krisito2015@abv.bg>
2023-05-01 20:30:57 +0300

    new commit

--- 1.2.24/Program.cs
+++ 1.2.24/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Diagnostics;
+using System.Drawing;
+using System.Reflection.Metadata;
 
 namespace _1._2._24
 {
@@ -6,8 +11,37 @@ namespace _1._2._24
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("new");
+            int h, m, s, moment;
 
+            h = int.Parse(Console.ReadLine());
+            if (h < 8)
+            {
+                h += 24;
+            }
+            m = int.Parse(Console.ReadLine());
+
+            s = int.Parse(Console.ReadLine());
+
+            moment = (3600 * h + 60 * m + s - 8 * 3600) % 108;
+
+            if (moment < 60)
+            {
+                Console.WriteLine("premini");
+            }
+            else if (moment < 64)
+            {
+                Console.WriteLine("izchakai");
+            }
+            else if (moment < 104)
+            {
+                Console.WriteLine("spri");
+            }
+            else
+            {
+                Console.WriteLine("izchakai");
+            }
+
+            return;
         }
     }
 }
